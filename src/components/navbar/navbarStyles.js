cd38f20091f1f6925f3fbe24670fb252ee5dcb7e
011Jorge <vicentejorge187@gmail.com>
2023-02-11 18:15:32 -0300

    :lipstick: feat: styling navbar

---
+++ src/components/navbar/navbarStyles.js
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  .border {
+    border: 1px solid red;
+  }
+
+  width: 100vw;
+  height: 350px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  font-family: "Roboto", sans-serif;
+
+  background-color: #00225b;
+`;
+
+export const ContainerNav = styled.div`
+  width: 90vw;
+  height: 300px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  .nav {
+    margin-top: 40px;
+  }
+
+  .nav a {
+    padding: 20px 40px;
+
+    font-size: 19px;
+
+    position: relative;
+    text-decoration: none;
+
+    color: #fff;
+  }
+
+  a:before {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 3px;
+    bottom: 0;
+    left: 0;
+    background-color: #89c43a;
+    visibility: hidden;
+    -webkit-transform: scaleX(0);
+    transform: scaleX(0);
+    -webkit-transition: all 0.3s ease-in-out 0s;
+    transition: all 0.3s ease-in-out 0s;
+  }
+
+  a:hover:before {
+    visibility: visible;
+    -webkit-transform: scaleX(1);
+    transform: scaleX(1);
+  }
+`;
